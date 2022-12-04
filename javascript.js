e93a19e5d3d3f2e7ce714fd806d39f2a9d815353
GrumpfGrincheux <117324547+GrumpfGrincheux@users.noreply.github.com>
2022-12-04 22:37:07 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -218,7 +218,7 @@ function selectOptions() {
   itemClassList.add(flexShrinkClasses);
   itemClassList.add(flexBasisClasses);
 
-  return { selectedItem };
+  return selectedItem;
 }
 
 function reinitValues(itemNumber) {
