db40d083a8c99b80c99061830b3bb388aa50389e
GrumpfGrincheux <117324547+GrumpfGrincheux@users.noreply.github.com>
2022-12-05 14:19:00 +0100

    Add files via upload

--- playscript.js
+++
@@ -1,9 +0,0 @@
-function apply() {
-    const height = document.querySelector("#height").value
-    const width = document.querySelector("#width").value
-
-    const container = document.querySelector(".container")
-    container.style = `width: ${width}px;
-                       height: ${height}px;`
-}
-
